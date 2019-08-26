8dfec355fb8b12ea09a469f9d428fba720c60d1c
Corey H <c.l.halicke@gmail.com>
2019-08-26 15:04:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -45,8 +45,6 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT
   projects.title AS project_title,
-  projects.funding_goal AS funding_goal,
-  SUM(pledges.amount) AS pledge_amount,
   SUM(pledges.amount) - projects.funding_goal AS amount_left
   FROM projects
   INNER JOIN pledges
