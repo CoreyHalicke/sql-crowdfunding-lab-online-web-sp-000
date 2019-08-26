e559ac6b4eeba0e8ecb13658ad231bb0f9bd9c65
Corey H <c.l.halicke@gmail.com>
2019-08-26 13:36:46 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -5,6 +5,16 @@
 # end
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
+=begin
+SELECT * FROM users;
+
+
+SELECT * FROM projects;
+
+
+SELECT * FROM pledges;
+
+=end
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, SUM(pledges.amount)
